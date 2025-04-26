a72fdbc3f0e31348d592876be19c503fc69d4a10
benny-lo <lorenzobenedetti1957@gmail.com>
2025-04-26 12:07:17 +0200

    corrected part1/setup_cluster.sh + created part3/setup_cluster.sh

--- part3/setup_cluster.sh
+++ part3/setup_cluster.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set up logging
-mkdir -p ../data/part1
-exec > >(tee -a ../data/part1/setup_log.txt) 2>&1
+mkdir -p ../data/part3
+exec > >(tee -a ../data/part3/setup_log.txt) 2>&1
 
 
 # Create the cluster
@@ -14,7 +14,7 @@ export KOPS_STATE_STORE=gs://cca-eth-2025-group-2-$username/
 export PROJECT=$(gcloud config get-value project)
 
 temp_config=$(mktemp)
-envsubst < part1.yaml > $temp_config
+envsubst < part3.yaml > $temp_config
 
 kops create -f $temp_config
 
@@ -22,10 +22,10 @@ kops create -f $temp_config
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 [ -f ~/.ssh/cloud-computing ] || ssh-keygen -t rsa -b 4096 -f ~/.ssh/cloud-computing
-kops create secret --name part1.k8s.local sshpublickey admin -i ~/.ssh/cloud-computing.pub
+kops create secret --name part3.k8s.local sshpublickey admin -i ~/.ssh/cloud-computing.pub
 
 # Deploy the cluster
-kops update cluster --name part1.k8s.local --yes --admin
+kops update cluster --name part3.k8s.local --yes --admin
 
 # Validate the cluster
 kops validate cluster --wait 10m
@@ -36,21 +36,11 @@ kubectl get nodes -o wide
 echo "Cluster is up and running"
 
 
-# Create the memcached pod
-echo "Setting up memcached now"
-
-kubectl create -f memcache-t1-cpuset.yaml
-kubectl expose pod some-memcached --name some-memcached-11211 \
---type LoadBalancer --port 11211 \
---protocol TCP
-sleep 60
-kubectl get service some-memcached-11211
-
-
 # Installing memcached
 echo "Installing memcached..."
 
-CLIENT_AGENT_NODE_NAME=$(kubectl get nodes --no-headers | grep client-agent | awk '{print $1}')
+CLIENT_AGENT_A_NODE_NAME=$(kubectl get nodes --no-headers | grep client-agent-a | awk '{print $1}')
+CLIENT_AGENT_B_NODE_NAME=$(kubectl get nodes --no-headers | grep client-agent-b | awk '{print $1}')
 CLIENT_MEASURE_NODE_NAME=$(kubectl get nodes --no-headers | grep client-measure | awk '{print $1}')
 
 # Function to install dependencies on a node
@@ -60,33 +50,35 @@ install_dependencies() {
   
   gcloud compute ssh --ssh-key-file ~/.ssh/cloud-computing "ubuntu@$NODE_NAME" --zone europe-west1-b \
     --command "
+      # Enable source repositories for build-dep
+      sudo sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
+      sudo apt-get update
+
       # Update package repositories
       sudo apt-get update
       
       # Install required development dependencies
-      sudo apt-get install -y libevent-dev libzmq3-dev git make g++ build-essential
+      sudo apt-get install libevent-dev libzmq3-dev git make g++ --yes
+      sudo apt-get build-dep memcached --yes
       
-      # Enable source repositories for build-dep
-      sudo sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources || true
-      sudo apt-get update
-      
-      # Clone and build memcache-perf
-      cd /home/ubuntu
-      if [ ! -d \"memcache-perf\" ]; then
-        git clone https://github.com/shaygalon/memcache-perf.git
-      fi
-      cd memcache-perf && git checkout 0afbe9b && make
+      # Clone and build memcache-perf-dynamic
+      git clone https://github.com/eth-easl/memcache-perf-dynamic.git
+      cd memcache-perf-dynamic
+      make
     "
 }
 
 # Install on both nodes in parallel
-install_dependencies "$CLIENT_AGENT_NODE_NAME" &
-AGENT_PID=$!
+install_dependencies "$CLIENT_AGENT_A_NODE_NAME" &
+AGENT_A_PID=$!
+
+install_dependencies "$CLIENT_AGENT_B_NODE_NAME" &
+AGENT_B_PID=$!
 
 install_dependencies "$CLIENT_MEASURE_NODE_NAME" &
 MEASURE_PID=$!
 
 # Wait for both installations to complete
 echo "Waiting for installations to complete..."
-wait $AGENT_PID $MEASURE_PID
+wait $AGENT_A_PID $AGENT_B_PID $MEASURE_PID
 echo "All installations finished!"
